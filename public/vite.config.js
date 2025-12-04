a55538982a56becf485aa13415091f1fde777212
coriawork <manuelcoriawork@gmail.com>
2025-12-04 20:00:59 -0300

    feat: actualizar dependencias y configurar proxy en Vite

--- public/vite.config.js
+++ public/vite.config.js
@@ -10,4 +10,19 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/functions/v1': {
+        target: 'https://srlgceodssgoifgosyoh.supabase.co',
+        changeOrigin: true,
+        secure: true,
+      },
+      '/email/v1': {
+        target: 'https://mvvuegssraetbyzeifov.supabase.co/functions',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/email\/v1/, '/v1')
+      },
+    },
+}
 })
